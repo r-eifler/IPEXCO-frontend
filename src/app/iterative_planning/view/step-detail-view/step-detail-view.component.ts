9574dda9a6c744ab50ab5bbbc6cbaf5384124c45
Merlin Köhler <merlin-koehler@t-online.de>
2024-10-05 20:43:27 +0200

    quote for natural languae representation of propery

--- src/app/iterative_planning/view/step-detail-view/step-detail-view.component.ts
+++ src/app/iterative_planning/view/step-detail-view/step-detail-view.component.ts
@@ -9,13 +9,14 @@ import { Store } from '@ngrx/store';
 import { BreadcrumbModule } from 'src/app/shared/component/breadcrumb/breadcrumb.module';
 import { PageModule } from 'src/app/shared/component/page/page.module';
 import { IterationStepHeroComponent } from '../../components/iteration-step-hero/iteration-step-hero.component';
+import { PlanProeprtyPanelComponent } from '../../components/plan-proeprty-panel/plan-proeprty-panel.component';
 import { selectIterativePlanningProperties, selectIterativePlanningSelectedStep } from '../../state/iterative-planning.selector';
 import { selectEnforcedGoals, selectSoftGoals, selectUnsolvableProperties } from './step-detail-view.component.selector';
 
 @Component({
   selector: 'app-step-detail-view',
   standalone: true,
-  imports: [PageModule, AsyncPipe, IterationStepHeroComponent, BreadcrumbModule, MatIconModule, RouterLink, MatButtonModule, MatTooltipModule],
+  imports: [PageModule, AsyncPipe, IterationStepHeroComponent, BreadcrumbModule, MatIconModule, RouterLink, MatButtonModule, MatTooltipModule, PlanProeprtyPanelComponent],
   templateUrl: './step-detail-view.component.html',
   styleUrl: './step-detail-view.component.scss'
 })
---
+++ src/app/shared/component/unformatted/unformatted/unformatted.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unformatted',
+  standalone: true,
+  imports: [],
+  templateUrl: './unformatted.component.html',
+  styleUrl: './unformatted.component.scss'
+})
+export class UnformattedComponent {
+
+}
