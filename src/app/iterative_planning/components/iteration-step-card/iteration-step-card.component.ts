bd679d2fa8ce94ad66591c126664676e0f93b0cd
Merlin Köhler <merlin-koehler@t-online.de>
2024-10-05 16:53:18 +0200

    add new plan actions

--- src/app/iterative_planning/components/iteration-step-card/iteration-step-card.component.ts
+++ src/app/iterative_planning/components/iteration-step-card/iteration-step-card.component.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterLink } from '@angular/router';
 
 import { DefaultPipe } from 'src/app/shared/common/pipe/default.pipe';
 import { LabelModule } from 'src/app/shared/component/label/label.module';
@@ -15,7 +16,7 @@ import { PlanProperty } from '../../domain/plan-property/plan-property';
 @Component({
   selector: 'app-iteration-step-card',
   standalone: true,
-  imports: [MatCardModule, MatChipsModule, StepStatusNamePipe, MatIconModule, LabelModule, StepValuePipe, DefaultPipe, MatButtonModule, RouterLink],
+  imports: [MatCardModule, MatChipsModule, StepStatusNamePipe, MatIconModule, LabelModule, StepValuePipe, DefaultPipe, MatButtonModule, RouterLink, MatTooltipModule],
   templateUrl: './iteration-step-card.component.html',
   styleUrl: './iteration-step-card.component.scss'
 })
