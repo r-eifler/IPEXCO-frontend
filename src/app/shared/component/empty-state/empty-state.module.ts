a9e87dda2d93b613f1113ec849d9a64fab5da363
Merlin Köhler <merlin-koehler@t-online.de>
2024-10-06 08:12:51 +0200

    empty state section

---
+++ src/app/shared/component/empty-state/empty-state.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { EmptyStateSectionComponent } from './empty-state-section/empty-state-section.component';
+
+
+
+@NgModule({
+  imports: [ EmptyStateSectionComponent ],
+  exports: [ EmptyStateSectionComponent ],
+})
+export class EmptyStateModule { }
