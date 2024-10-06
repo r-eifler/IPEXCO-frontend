746bfea6b96c45278d2ebbed46ef5cc9c456c097
Merlin Köhler <merlin-koehler@t-online.de>
2024-10-06 11:16:22 +0200

    use outlined icons

--- src/app/iterative_planning/view/create-iteration/create-iteration.component.ts
+++ src/app/iterative_planning/view/create-iteration/create-iteration.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
+import { InfoModule } from 'src/app/shared/component/info/info.module';
 import { SideSheetModule } from 'src/app/shared/component/side-sheet/side-sheet.module';
 
 @Component({
   selector: 'app-create-iteration',
   standalone: true,
-  imports: [SideSheetModule],
+  imports: [SideSheetModule, InfoModule, MatIconModule],
   templateUrl: './create-iteration.component.html',
   styleUrl: './create-iteration.component.scss'
 })
