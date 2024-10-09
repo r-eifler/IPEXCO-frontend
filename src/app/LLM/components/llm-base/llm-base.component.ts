bd618f0d66e4ef3ae067a12767c229809a45cdfa
gfouilhe <g.g.a.fouilhe@gmail.com>
2024-10-09 09:52:01 +0200

    reverted component changes + new LLM actions

--- src/app/LLM/components/llm-base/llm-base.component.ts
+++ src/app/LLM/components/llm-base/llm-base.component.ts
@@ -2,7 +2,6 @@ import { Component, inject } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { sendMessageToLLM } from '../../state/llm.actions';
 import { selectIsLoading, selectMessages } from './llm-base.component.selector';
-import { TranslatorRequest,QuestionTranslationRequest, GoalTranslationRequest, ExplanationTranslationRequest } from '../../translators_interfaces';
 
 @Component({
   selector: 'app-llm-base',
@@ -16,13 +15,6 @@ export class LlmBaseComponent {
   isLoading$ = this.store.select(selectIsLoading);
 
   onUserMessage(request: string) {
-    const qt_request: QuestionTranslationRequest = {
-      question: request,
-      enforcedGoals: [],  
-      satisfiedGoals: [],
-      unsatisfiedGoals: [],
-      existingPlanProperties: []
-    }
-    this.store.dispatch(sendMessageToLLM({ request: qt_request, endpoint: 'qt' } as TranslatorRequest))
+    this.store.dispatch(sendMessageToLLM({ request }))
   }
 }
--- src/app/LLM/state/llm.actions.ts
+++ src/app/LLM/state/llm.actions.ts
@@ -1,7 +1,12 @@
 import { createAction, props } from "@ngrx/store";
-import { TranslatorRequest } from "../translators_interfaces";
+import { QuestionTranslationRequest, GoalTranslationRequest, ExplanationTranslationRequest } from "../translators_interfaces";
 
-export const sendMessageToLLM = createAction('[llm] send message', props<TranslatorRequest>());
-export const sendMessageToLLMSuccess = createAction('[llm] send message success', 
-    props<{response: string}>());
+export const sendMessageToLLM = createAction('[llm] send message', props<{request: string}>());
+export const sendMessageToLLMSuccess = createAction('[llm] send message success',  props<{response: string}>());
 export const sendMessageToLLMFailure = createAction('[llm] send message failure');
+
+export const eraseLLMHistory = createAction('[llm] erase history');
+
+export const sendMessageToLLMQuestionTranslator = createAction('[llm] send message to question translator', props<QuestionTranslationRequest>());
+export const sendMessageToLLMGoalTranslator = createAction('[llm] send message to goal translator', props<GoalTranslationRequest>());
+export const sendMessageToLLMExplanationTranslator = createAction('[llm] send message to explanation translator', props<ExplanationTranslationRequest>());
