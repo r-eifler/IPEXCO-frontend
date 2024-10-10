46853621a3b62eaf19b8c7349ac9bc618b8614ff
gfouilhe <g.g.a.fouilhe@gmail.com>
2024-10-10 10:43:53 +0200

    improvements on llm service and selector\

--- src/app/LLM/service/llm.service.ts
+++ src/app/LLM/service/llm.service.ts
@@ -5,8 +5,10 @@ import { environment } from "src/environments/environment";
 import { map, tap } from "rxjs/operators";
 import { IHTTPData } from "src/app/interface/http-data.interface";
 import { Message } from "../domain/message";
-import { TranslationHistory } from "../translators_interfaces";
-import templates from "../templates.json"
+import { ExplanationTranslationRequest, GoalTranslationRequest, QuestionTranslationRequest, GoalTranslationResponse, QuestionTranslationResponse,ExplanationTranslationResponse, QuestionTranslatorHistory, ExplanationTranslatorHistory } from "../translators_interfaces";
+import * as templates from "../templates.json"
+import { ofType } from "@ngrx/effects";
+import { isTypeElement } from "typescript";
 
 @Injectable()
 export class LLMService{
@@ -17,7 +19,16 @@ export class LLMService{
     
     postMessage$(messages: Message[] | TranslationHistory, endpoint: string): Observable<string> {
         endpoint = endpoint ?? 'simple';
-        endpoint = 'qt'
+
+        if(endpoint === 'gt'){
+            messages = prepareGoalTranslatorMessage(messages as GoalTranslationHistory);
+        }
+        else if(endpoint === 'qt'){
+            messages = prepareQuestionTranslatorMessage(messages as QuestionTranslationRequest);
+        }
+        else if(endpoint === 'et'){
+            messages = prepareExplanationTranslatorMessage(messages as ExplanationTranslationRequest);
+        }
         console.log(messages);
         return this.http.post<IHTTPData<string>>(this.BASE_URL + endpoint, { data: messages }).pipe(
             map(({ data }) => data),
@@ -31,15 +42,25 @@ function prepareGoalTranslatorMessage(input: GoalTranslationRequest): string {
   
     // Replace placeholders with actual values
     return promptTemplate
+      .replace('{goal}', input.goalDescription)
       .replace('{predicates}', JSON.stringify(input.predicates))
-      .replace('{objects}', JSON.stringify(input.objects));
-  }
+      .replace('{objects}', JSON.stringify(input.objects))
+      .replace('{existing_plan_properties}', JSON.stringify(input.existingPlanProperties));
+}
+  
+function prepareGoalTranslatorResponse(response: GoalTranslatorResponse): string {
+    const formula = 
+
+}
   
   function prepareQuestionTranslatorMessage(input: QuestionTranslationRequest): string {
     const promptTemplate = templates.question_translator;
     return promptTemplate
       .replace('{question}', input.question)
-  
+      .replace('{enforced_goals}', JSON.stringify(input.enforcedGoals))
+      .replace('{satisfied_goals}', JSON.stringify(input.satisfiedGoals))
+      .replace('{unsatisfied_goals}', JSON.stringify(input.unsatisfiedGoals))
+      .replace('{existing_plan_properties}', JSON.stringify(input.existingPlanProperties));
   }
   
   function prepareExplanationTranslatorMessage(input: ExplanationTranslationRequest): string {
@@ -47,5 +68,25 @@ function prepareGoalTranslatorMessage(input: GoalTranslationRequest): string {
   
     return promptTemplate
       .replace('{question}', input.question)
-      
+      .replace('{question_type}', input.question_type)
+      .replace('{question_arguments}', JSON.stringify(input.questionArguments))
+      .replace('{MUGS}', JSON.stringify(input.MUGS))
+      .replace('{MGCS}', JSON.stringify(input.MGCS))
+      .replace('{enforced_goals}', JSON.stringify(input.enforcedGoals))
+      .replace('{satisfied_goals}', JSON.stringify(input.satisfiedGoals))
+      .replace('{unsatisfied_goals}', JSON.stringify(input.unsatisfiedGoals))
+      .replace('{existing_plan_properties}', JSON.stringify(input.existingPlanProperties));
   }
+
+function prepareQuestionTranslatorHistory(history: QuestionTranslatorHistory): Message[] {
+    let messages: Message[] ;
+    for (const step of history) {
+        const input = step.input;
+        const output = step.output;
+        const input_message = prepareQuestionTranslatorMessage(input);
+
+        messages.push({role: "user", content: input_message});
+        messages.push({role: "assistant", content: output});
+    }
+    return messages
+}
--- src/app/LLM/state/llm.selector.ts
+++ src/app/LLM/state/llm.selector.ts
@@ -1,8 +1,10 @@
 import { createFeatureSelector, createSelector } from "@ngrx/store";
 import { LLMChatFeature, LLMChatState } from "./llm.reducer";
+import { IterativePlanningState } from "src/app/iterative_planning/state/iterative-planning.reducer";
 
-
+export const IterativePlanningFeature = 'it_planning';
 const selectLLMChatFeature = createFeatureSelector<LLMChatState>(LLMChatFeature);
+const selectIterativePlanningFeature = createFeatureSelector<IterativePlanningState>(IterativePlanningFeature);
 
 export const selectMessages = createSelector(selectLLMChatFeature, (state) => state.messages)
 export const selectLoadingState = createSelector(selectLLMChatFeature, ({ loadingState }) => loadingState );
