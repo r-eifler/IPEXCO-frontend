56950a4655e717dba03f38ef430a8407393e1f4d
gfouilhe <g.g.a.fouilhe@gmail.com>
2024-11-08 14:36:04 +0100

    comments

--- src/app/iterative_planning/domain/explanation/explanations.ts
+++ src/app/iterative_planning/domain/explanation/explanations.ts
@@ -1,11 +1,11 @@
-
 export enum QuestionType {
-  WHY_PLAN = 'WHY_PLAN',
-  HOW_PLAN = 'HOW_PLAN',
-  WHY_NOT_PROPERTY = 'WHY_NOT_PROPERTY',
-  WHAT_IF_PROPERTY = 'WHAT_IF_PROPERTY',
-  CAN_PROPERTY = 'CAN_PROPERTY',
-  HOW_PROPERTY = 'HOW_PROPERTY',
+  WHY_PLAN = 'WHY_PLAN', // Why is the task unsolvable?
+  HOW_PLAN = 'HOW_PLAN', // How can I make the task solvable?
+  WHY_NOT_PROPERTY = 'WHY_NOT_PROPERTY',// Why are Q not satisfied?
+  WHAT_IF_PROPERTY = 'WHAT_IF_PROPERTY', // What happens if we enforce Q?
+  CAN_PROPERTY = 'CAN_PROPERTY', // Can Q be satisfied?
+  HOW_PROPERTY = 'HOW_PROPERTY', // How can Q be satisfied?
+ 
 }
 
 export enum AnswerType {
