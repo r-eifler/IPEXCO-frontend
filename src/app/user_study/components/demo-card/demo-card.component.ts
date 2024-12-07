50141a511ad7907fb4f4b5b19a6baa96756055f5
Rebecca Eifler <eifler@cs.uni-saarland.de>
2024-12-07 19:54:56 +0100

    description card for user study

---
+++ src/app/user_study/components/demo-card/demo-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './demo-card.component.html',
+  styleUrl: './demo-card.component.scss'
+})
+export class DemoCardComponent {
+
+}
