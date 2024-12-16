e195b7663a45c7aa847d91f804bf3c5bacd685ee
Rebecca Eifler <eifler@cs.uni-saarland.de>
2024-12-16 18:33:01 +0100

    fix / and NginX config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiURL: "http://localhost:4200/api",
-  localURL: "http://localhost:4200",
+  apiURL: "http://localhost:4200/api/",
+  localURL: "http://localhost:4200/",
   srcURL: "http://localhost:4200",
-  // apiURL: "https://ipexco.irtse.com/api",
-  // localURL: "https://ipexco.irtse.com",
+  // apiURL: "https://ipexco.irtse.com/api/",
+  // localURL: "https://ipexco.irtse.com/",
   // srcURL: "https://ipexco.irtse.com",
 };
