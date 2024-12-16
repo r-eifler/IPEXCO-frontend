f46e9e32f09f7a252564a23874593d84b5e9b156
Rebecca Eifler <eifler@cs.uni-saarland.de>
2024-12-16 19:48:13 +0100

    prod environment use same host for api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: "http://localhost:4200/api/",
-  localURL: "http://localhost:4200/",
-  srcURL: "http://localhost:4200",
-  // apiURL: "https://ipexco.irtse.com/api/",
-  // localURL: "https://ipexco.irtse.com/",
-  // srcURL: "https://ipexco.irtse.com",
+  apiURL: "/api/",
+  localURL: "",
+  srcURL: "",
 };
