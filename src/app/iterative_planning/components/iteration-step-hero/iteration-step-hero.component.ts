8f8011d6f2d550a6e708e05bae32e1e7ed22efd0
Rebecca Eifler <eifler@cs.uni-saarland.de>
2025-01-09 11:25:52 +0100

    add max utility to step detail view during demo

--- src/app/iterative_planning/components/iteration-step-hero/iteration-step-hero.component.ts
+++ src/app/iterative_planning/components/iteration-step-hero/iteration-step-hero.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
@@ -14,14 +14,37 @@ import { StepStatusNamePipe } from '../../domain/pipe/step-status-name.pipe';
 import { StepValuePipe } from '../../domain/pipe/step-value.pipe';
 import { PlanProperty } from '../../../shared/domain/plan-property/plan-property';
 import { UserStudyDirective } from '../../derectives/isUserStudy.directive';
+import { maxUtility } from 'src/app/project/domain/demo';
+import { Demo } from 'src/app/shared/domain/demo';
+import { ProjectDirective } from '../../derectives/isProject.directive';
+import { DemoDirective } from '../../derectives/isDemo.directive';
 
 @Component({
     selector: 'app-iteration-step-hero',
-    imports: [MatCardModule, MatChipsModule, StepStatusNamePipe, MatIconModule, LabelModule, StepValuePipe, DefaultPipe, MatButtonModule, RouterLink, MatTooltipModule, StepStatusColorPipe, RouterLink, UserStudyDirective],
+    imports: [
+      MatCardModule, 
+      MatChipsModule, 
+      StepStatusNamePipe, 
+      MatIconModule, 
+      LabelModule, 
+      StepValuePipe, 
+      DefaultPipe, 
+      MatButtonModule, 
+      RouterLink, 
+      MatTooltipModule, 
+      StepStatusColorPipe,
+      RouterLink, 
+      UserStudyDirective,
+      ProjectDirective,
+      DemoDirective
+    ],
     templateUrl: './iteration-step-hero.component.html',
     styleUrl: './iteration-step-hero.component.scss'
 })
 export class IterationStepHeroComponent {
+  demo = input.required<Demo>();
   step = input.required<IterationStep | null>();
   planProperties = input.required<Record<string, PlanProperty> | null>();
+
+   maxOverallUtility = computed(() => maxUtility(this.demo(), this.planProperties() ? Object.values(this.planProperties()) : null))
 }
