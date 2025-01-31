e12eb4aefa1e894e0328f972c1605a7bcb54a349
Rebecca Eifler <eifler@cs.uni-saarland.de>
2025-01-31 23:30:19 +0100

    partically fix lmm config in settings

--- src/app/project/components/settings/settings.component.ts
+++ src/app/project/components/settings/settings.component.ts
@@ -55,6 +55,10 @@ export class SettingsComponent {
   explanationTransInstructionPrompts = computed(() => this.prompts()?.filter(p => p.type == PromptType.INSTRUCTION_AND_EXAMPLES && p.agent == AgentType.EXPLANATION_TRANSLATOR));
   explanationTransDataPrompts = computed(() => this.prompts()?.filter(p => p.type == PromptType.INPUT_DATA && p.agent == AgentType.EXPLANATION_TRANSLATOR));
 
+  explanationTransOutputSchemas = computed(() => this.outputSchemas()?.filter(p => p.agent == AgentType.EXPLANATION_TRANSLATOR));
+  goalTransOutputSchemas = computed(() => this.outputSchemas()?.filter(p => p.agent == AgentType.GOAL_TRANSLATOR));
+  questionClassOutputSchemas = computed(() => this.outputSchemas()?.filter(p => p.agent == AgentType.QUESTION_CLASSIFIER));
+
   update = output<GeneralSettings>();
 
   ExplanationTypes = ExplanationInterfaceType;
@@ -82,8 +86,20 @@ export class SettingsComponent {
       model:  this.fb.control<string>('gpt-4o-mini', Validators.required),
       temperature:  this.fb.control<number>(0, [Validators.required, Validators.min(0), Validators.max(2)]),
       maxCompletionTokens: this.fb.control<number | null>(null, Validators.required),
-      prompts: this.fb.control<string[]>([]),
-      outputSchema:  this.fb.control<string[]>([]),
+      prompts: this.fb.group({
+        system: this.fb.control<string>(null),
+        goalTransInstructions: this.fb.control<string>(null),
+        goalTransData: this.fb.control<string>(null),
+        questionClassInstructions: this.fb.control<string>(null),
+        questionClassData: this.fb.control<string>(null),
+        explanationTransInstruction: this.fb.control<string>(null),
+        explanationTransData: this.fb.control<string>(null),
+      }),
+      outputSchemas:  this.fb.group({
+        goalTrans: this.fb.control<string>(null),
+        questionClass: this.fb.control<string>(null),
+        explanationTrans: this.fb.control<string>(null),
+      }),
     }),
     userStudy: this.fb.group({
           introTask: this.fb.control<boolean>(false, Validators.required),
@@ -120,6 +136,15 @@ export class SettingsComponent {
     this.form.controls.services.controls.computeExplanationsAutomatically.setValue(settings.services.computeExplanationsAutomatically);
     this.form.controls.services.controls.planners.setValue(settings.services.planners);
     this.form.controls.services.controls.explainer.setValue(settings.services.explainer);
+
+    this.form.controls.llmConfig.controls.model.setValue(settings.llmConfig.model);
+    this.form.controls.llmConfig.controls.temperature.setValue(settings.llmConfig.temperature);
+    this.form.controls.llmConfig.controls.maxCompletionTokens.setValue(settings.llmConfig.maxCompletionTokens);
+
+    //TODO
+    this.form.controls.llmConfig.controls.prompts.controls.system.setValue(this.systemPrompts()?.find(p => settings.llmConfig.prompts.includes(p._id))._id)
+    //TODO other prompt types
+    // TODO output schemas
 
     this.form.controls.userStudy.controls.introTask.setValue(settings.userStudy.introTask);
     this.form.controls.userStudy.controls.checkMaxUtility.setValue(settings.userStudy.checkMaxUtility);
@@ -161,8 +186,20 @@ export class SettingsComponent {
         model: this.form.controls.llmConfig.controls.model.value,
         temperature: this.form.controls.llmConfig.controls.temperature.value,
         maxCompletionTokens: this.form.controls.llmConfig.controls.maxCompletionTokens.value,
-        prompts: this.form.controls.llmConfig.controls.prompts.value,
-        outputSchema: this.form.controls.llmConfig.controls.outputSchema.value,
+        prompts: [
+          this.form.controls.llmConfig.controls.prompts.controls.system.value,
+          this.form.controls.llmConfig.controls.prompts.controls.goalTransInstructions.value,
+          this.form.controls.llmConfig.controls.prompts.controls.goalTransData.value,
+          this.form.controls.llmConfig.controls.prompts.controls.questionClassInstructions.value,
+          this.form.controls.llmConfig.controls.prompts.controls.questionClassData.value,
+          this.form.controls.llmConfig.controls.prompts.controls.explanationTransInstruction.value,
+          this.form.controls.llmConfig.controls.prompts.controls.explanationTransData.value,
+        ].filter(e => !!e),
+        outputSchema: [
+          this.form.controls.llmConfig.controls.outputSchemas.controls.goalTrans.value,
+          this.form.controls.llmConfig.controls.outputSchemas.controls.questionClass.value,
+          this.form.controls.llmConfig.controls.outputSchemas.controls.explanationTrans.value,
+        ]
       },
       userStudy: {
           introTask: this.form.controls.userStudy.controls.introTask.value,
