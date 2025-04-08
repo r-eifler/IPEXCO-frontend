7dff3244a6e77ec5dc8236b1253a3b0bacf279bc
Rebecca Eifler <eifler@cs.uni-saarland.de>
2025-04-08 14:27:56 +0200

    action type in iter step unknwn

--- src/app/iterative_planning/domain/plan.ts
+++ src/app/iterative_planning/domain/plan.ts
@@ -2,7 +2,7 @@
 import { sum } from "ramda";
 import { factEquals, PDDLAction, PDDLFact } from "src/app/shared/domain/PDDL_task";
 import { Action, ActionZ, PlanProperty } from "src/app/shared/domain/plan-property/plan-property";
-import { array, coerce, date, nativeEnum, object, string, infer as zinfer } from "zod";
+import { array, coerce, date, nativeEnum, object, string, unknown, infer as zinfer } from "zod";
 
 
 export enum PlanRunStatus {
@@ -20,7 +20,7 @@ export const PlanRunStatusZ = nativeEnum(PlanRunStatus);
 export const PlanZ = object({
   createdAt: coerce.date(), 
   status: PlanRunStatusZ,
-  actions: array(ActionZ).nullish(),
+  actions: array(unknown()).nullish(),
   satisfied_properties: array(string()).optional(),
 });
 
