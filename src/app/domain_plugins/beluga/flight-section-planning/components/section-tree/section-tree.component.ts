196e142f5f2a87681bf7e4a46604469e54933bc9
Rebecca Eifler <eifler@cs.uni-saarland.de>
2025-05-17 23:40:33 +0200

    and check next flight button needed

--- src/app/domain_plugins/beluga/flight-section-planning/components/section-tree/section-tree.component.ts
+++ src/app/domain_plugins/beluga/flight-section-planning/components/section-tree/section-tree.component.ts
@@ -1,11 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, effect, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { TranslocoModule } from '@jsverse/transloco';
 import { Store } from '@ngrx/store';
 import { FlightSection } from '../../domain/flight-section';
 import { createSuccessorFlightSection } from '../../state/flight-section-planning.actions';
-import { selectActiveBranchLastSectionFinished, selectActiveBranchSections, selectFlights } from '../../state/flight-section-planning.selector';
+import { selectActiveBranchLastSectionFinished, selectActiveBranchNumberFinishedFlights, selectActiveBranchSections, selectFlights, selectNumFlights } from '../../state/flight-section-planning.selector';
 import { SectionCardComponent } from '../section-card/section-card.component';
+
 
 
 
@@ -27,7 +28,16 @@ export class SectionTreeComponent {
   flights = this.store.selectSignal(selectFlights);
   lastSectionFinished = this.store.selectSignal(selectActiveBranchLastSectionFinished);
 
+  numFlights = this.store.selectSignal(selectNumFlights);
+  numFinishedFlights = this.store.selectSignal(selectActiveBranchNumberFinishedFlights);
+
   onNextFlightPlan(section: FlightSection){
     this.store.dispatch(createSuccessorFlightSection({section}))
+  }
+
+  hasNextFlight = computed(() => this.numFlights() > (this.numFinishedFlights() ?? 0))
+
+  constructor(){
+    effect(() => console.log("hasNext:" + this.hasNextFlight()))
   }
 }
