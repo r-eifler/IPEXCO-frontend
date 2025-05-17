48008c339f4174e6cc93066cf5765aacd7404f2a
Merlin Köhler <merlin-koehler@t-online.de>
2025-05-17 16:01:29 +0200

    include undo button in tools section

--- src/app/shared/state/undo/undo-stack.service.ts
+++ src/app/shared/state/undo/undo-stack.service.ts
@@ -1,31 +1,39 @@
-import { inject, Injectable, InjectionToken } from "@angular/core";
+import { computed, inject, Injectable, InjectionToken, signal } from "@angular/core";
 import { Action } from "@ngrx/store";
+import { any, slice } from "ramda";
 
 export const UndoStackBufferSize = new InjectionToken<number>('UNDO_STACK_BUFFER_SIZE');
 
 @Injectable()
 export class UndoStackService {
-  private stack: Action[] = [];
+  private stack = signal<Action[]>([]);
 
   private bufferSize = inject(UndoStackBufferSize, { optional: true }) ?? 100;
 
+  hasStackItems = computed(() => any(() => true ,this.stack()));
+
   push(action: Action) {
-    this.stack.push(action);
+    this.stack.update(oldStack => [...oldStack, action]);
 
     this.adjustBuffer();
-    console.log(this.stack)
   }
 
   pop() {
-    return this.stack.pop();
+    const lastAction = this.stack().at(-1);
+
+    this.stack.update(oldStack => slice(0, oldStack.length - 1, oldStack));
+
+    console.log(this.stack())
+
+    return lastAction;
   }
 
   private adjustBuffer() {
-      if (! this.bufferSize || this.stack.length !== this.bufferSize + 1) {
+      if (! this.bufferSize || this.stack().length !== this.bufferSize + 1) {
         return;
       }
 
-      this.stack = this.stack.slice(1, this.bufferSize + 1);
+      this.stack.update(oldStack => slice(1, this.bufferSize + 1, oldStack));
   }
 
 }
