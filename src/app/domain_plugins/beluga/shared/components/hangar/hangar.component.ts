3f6914db585158304040723f9060a5be729db59e
Merlin Köhler <merlin-koehler@t-online.de>
2025-05-17 23:55:51 +0200

    drag indication

--- src/app/domain_plugins/beluga/shared/components/hangar/hangar.component.ts
+++ src/app/domain_plugins/beluga/shared/components/hangar/hangar.component.ts
@@ -4,7 +4,7 @@ import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-hangar',
   imports: [
-    MatIconModule
+    MatIconModule,
   ],
   templateUrl: './hangar.component.html',
   styleUrl: './hangar.component.scss'
