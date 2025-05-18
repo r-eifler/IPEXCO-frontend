e570f2a4eb9d0a6968bf1ffd2dd9d9298261dc1c
Rebecca Eifler <eifler@cs.uni-saarland.de>
2025-05-18 15:50:21 +0200

    user code brefore study start

--- src/app/user_study_execution/view/user-study-execution-start-view/user-study-execution-start-view.component.ts
+++ src/app/user_study_execution/view/user-study-execution-start-view/user-study-execution-start-view.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from '@angular/core';
+import {Component, computed, inject, signal, WritableSignal} from '@angular/core';
 import {PageModule} from '../../../shared/components/page/page.module';
 import {Store} from '@ngrx/store';
 import {selectExecutionUserStudy} from '../../state/user-study-execution.selector';
@@ -11,6 +11,9 @@ import { take } from 'rxjs';
 import { executionSaveProlificId } from '../../state/user-study-execution.actions';
 import { TranslocoModule } from '@jsverse/transloco';
 import { changeLanguage } from 'src/app/user/state/user.actions';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
     selector: 'app-user-study-execution-start-view',
@@ -22,6 +25,9 @@ import { changeLanguage } from 'src/app/user/state/user.actions';
         InfoComponent,
         MatIconModule,
         TranslocoModule,
+        MatFormField,
+        MatInputModule,
+        FormsModule,
     ],
     templateUrl: './user-study-execution-start-view.component.html',
     styleUrl: './user-study-execution-start-view.component.scss'
@@ -31,6 +37,8 @@ export class UserStudyExecutionStartViewComponent {
   store = inject(Store);
 
   activatedRoute = inject(ActivatedRoute);
+  userCode: WritableSignal<string|null> = signal(null);
+  hasCode = computed(() => this.userCode() !== null && (this.userCode()?.length ?? 0) > 3)
 
   userStudy$ = this.store.select(selectExecutionUserStudy);
 
@@ -41,6 +49,13 @@ export class UserStudyExecutionStartViewComponent {
         this.store.dispatch(executionSaveProlificId({id}));
       }
     })
+  }
+
+  storeCode(){
+    const code = this.userCode();
+    if(code !== null){
+      this.store.dispatch(executionSaveProlificId({id: code}));
+    }
   }
   
   setLanguage(code: string){
