0103657cdbe3e2cfa8f76ab468acac777bbaa098
gfouilhe <g.g.a.fouilhe@gmail.com>
2025-06-04 16:53:04 +0200

    3 random questions, refresh button on top

--- src/app/iterative_planning/view/step-detail-view/step-detail-view.component.ts
+++ src/app/iterative_planning/view/step-detail-view/step-detail-view.component.ts
@@ -5,7 +5,7 @@ import { MatIconModule } from "@angular/material/icon";
 import { MatTooltipModule } from "@angular/material/tooltip";
 import { ActivatedRoute, Router, RouterLink } from "@angular/router";
 import { Store } from "@ngrx/store";
-import { Observable, combineLatest, filter, map, of, switchMap, take } from "rxjs";
+import { Observable, Subject, combineLatest, filter, map, of, startWith, switchMap, take } from "rxjs";
 
 import { BreadcrumbModule } from "src/app/shared/components/breadcrumb/breadcrumb.module";
 import { EmptyStateModule } from "src/app/shared/components/empty-state/empty-state.module";
@@ -194,13 +194,30 @@ export class StepDetailViewComponent {
   }
 
   combinedPropertyAvailableQuestionTypes$: Observable<AvailableQuestion[]> = this.unsolvedSoftGoals$.pipe(
-    map(properties => properties.slice(0, 3)), // Take the first 3 properties
     switchMap(properties => {
       if (properties.length === 0) {
         return of([]); // If no properties, return an empty array
       }
-      // Take the available questions from the first property
-      return this.propertyAvailableQuestionTypes$(properties[0]);
+      return combineLatest(
+        properties.map(property => this.propertyAvailableQuestionTypes$(property))
+      ).pipe(
+        map(arrayOfArrays => arrayOfArrays.flat()) // Flatten the array of arrays
+      );
+    })
+  );
+
+  private refreshSuggestedQuestionsSubject = new Subject<void>();
+  refreshSuggestedQuestions$ = this.refreshSuggestedQuestionsSubject.asObservable();
+
+  selectedCombinedPropertyAvailableQuestionTypes$: Observable<AvailableQuestion[]> = combineLatest([
+    this.combinedPropertyAvailableQuestionTypes$,
+    this.refreshSuggestedQuestions$.pipe(startWith(undefined))
+  ]).pipe(
+    map(([questions, _]) => {
+      // Shuffle the array of questions
+      const shuffledQuestions = questions.sort(() => 0.5 - Math.random());
+      // Take the first 3 questions
+      return shuffledQuestions.slice(0, 3);
     })
   );
 
@@ -238,6 +255,9 @@ export class StepDetailViewComponent {
     });
   }
 
+  onRefreshSuggestedQuestions(): void {
+    this.refreshSuggestedQuestionsSubject.next();
+  }
 
   onHelp(){
     this.dialog.open(UserManualDialogComponent);
